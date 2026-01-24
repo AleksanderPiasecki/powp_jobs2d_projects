bd0e0b0fe8ef2ee32ff7c1dfc3e017967915e016
Koisad <patrykpaczkow@gmail.com>
2026-01-24 14:34:25 +0100

    feat: add Transformation Visitor and command coordinates getters

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -34,4 +34,12 @@ public class OperateToCommand implements DriverCommand {
     public void accept(CommandVisitor visitor) {
         visitor.visit(this);
     }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
 }
